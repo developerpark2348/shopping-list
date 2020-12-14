c9295ffb6b6d4eb9c11ff8e4e706ff2c1b4bcc2d
developerpark2348 <developerpark2348@gmail.com>
2020-12-14 11:41:09 +0900

    only html,css to buy
    my shopping list html, css

---
+++ main.js
@@ -0,0 +1,6 @@
+function add() {
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode());
+    ul.appendChild(li);
+}
